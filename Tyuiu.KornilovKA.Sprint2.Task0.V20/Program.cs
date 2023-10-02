2bb1f2e38661ac0a792f7414e1d6de534f7a30c9
kilianAid <kilian_bs@mail.ru>
2023-10-02 13:26:20 +0500

    Add project files.

---
+++ Tyuiu.KornilovKA.Sprint2.Task0.V20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KornilovKA.Sprint2.Task0.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
