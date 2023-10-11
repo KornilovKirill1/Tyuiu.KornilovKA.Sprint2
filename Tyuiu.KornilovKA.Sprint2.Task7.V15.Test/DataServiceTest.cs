46bbf4490096c20febd89da2bb792baa2a6084bf
kilianAid <kilian_bs@mail.ru>
2023-10-11 15:00:25 +0500

    Task7

---
+++ Tyuiu.KornilovKA.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KornilovKA.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0;
+            double y = 2;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.KornilovKA.Sprint2.Task7.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KornilovKA.Sprint2.Task7.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KornilovKA.Sprint2.Task7.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89d2d768-30af-44d2-b351-f3bd66367dd5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
